5384b0fe9ae51d2eea50a707a2da6ff9ffc775b0
Vex1re <ivanovmatvej4548@gmail.com>
2026-01-27 19:44:23 +0400

    NavBar + Another classes

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,5 +1,7 @@
 import './style.css'
 
+// ПЛАВНОЕ ПОЯВЛЕНИЕ ПРИ СКРОЛЛЕ
+
 const reveals = document.querySelectorAll(".reveal");
 
 const revealOnScroll = () => {
@@ -17,3 +19,28 @@ const revealOnScroll = () => {
 
 window.addEventListener("scroll", revealOnScroll);
 revealOnScroll();
+
+// ДВИЖЕНИЕ navbar
+
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    navbar.classList.add(
+      "py-3",
+      "backdrop-blur-xl",
+      "bg-red-950/30",
+      "shadow-[0_20px_60px_rgba(0,0,0,0.7)]"
+    );
+    navbar.classList.remove("py-5");
+  } else {
+    navbar.classList.remove(
+      "py-3",
+      "backdrop-blur-xl",
+      "bg-red-950/80",
+      "shadow-[0_20px_60px_rgba(0,0,0,0.7)]"
+    );
+    navbar.classList.add("py-5");
+  }
+});
+
